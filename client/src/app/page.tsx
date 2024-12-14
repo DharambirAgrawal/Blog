bcb09bc10393efba66857c388b58bcf2289123d7
Dharambirbro <dev.dharambir@gmail.com>
2024-12-14 11:40:17 -0600

    first commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import RichTextEditor from "@/components/Dashboard/TextEditor";
+export default function Home() {
+  return (
+    <div>
+      <RichTextEditor />
+    </div>
+  );
+}

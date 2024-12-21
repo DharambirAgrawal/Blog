5898c08b586f0f3a3f990a1ca2b17d4d9aae2e36
Dharambir Agrawal <dev.dharambir@gmail.com>
2024-12-20 19:14:43 -0800

    Addid subscribe and updated prisma

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,7 +1,9 @@
 import asyncHandler from "express-async-handler";
 import { AppError } from "../errors/AppError.js";
 import { validateEmail } from "../utils/utils.js";
-
+import { SUBSCRIPTION_MESSAGE } from "../messages/emailMessage.js";
+import { sendEmail } from "../services/emailService.js";
+import { prisma } from "../../app.js";
 export const subscribeUser = asyncHandler(async (req, res) => {
   const { email } = req.body;
   if (!email) {
@@ -10,6 +12,15 @@ export const subscribeUser = asyncHandler(async (req, res) => {
   if (!validateEmail(email)) {
     throw new AppError("Please enter valid email", 400);
   }
-  console.log(email);
-  res.send("gg");
+  const subscription = await prisma.subscription.create({
+    data: {
+      email,
+    },
+  });
+  if (!subscription) {
+    throw new AppError("Error subscribing user", 500);
+  }
+  await sendEmail(email, SUBSCRIPTION_MESSAGE());
+
+  res.status(200).json({ message: "Subscribed successfully" });
 });
